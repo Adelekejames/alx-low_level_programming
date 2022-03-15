3322a68a6125a5e52d90f0c491af81411d5298b2
Adeleke ololade James <47466668+Adelekejames@users.noreply.github.com>
2022-03-15 23:31:34 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,10 @@
  * Description: It prints the word _putchar, followed by a new line.
  * Return: 0.
  */
-char main(void)
+int main(void)
 {
 	char word[8] = "_putchar";
 	int i;
-
 	for (i = 0; i < 8; i++)
 		_putchar(word[i]);
 	_putchar('\n');
